de275a16f04e5e87288686b296d3a89638655248
Becca <rhamman@stu.ctcd.edu>
2017-04-19 17:28:40 -0500

    Solved Java String-1

--- driver.cpp
+++ driver.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
 #include <string>
+#include <cassert>
 using namespace std;
+
+string helloName(string name);
 
 int main()
 {
 	cout << "Hello World!" << endl;
     cout << "This is a great day to code!" << endl;
 
-
+    
+    assert(helloName("Bob") == "Hello Bob!");
+    assert(helloName("Alice") == "Hello Alice!");
+    assert(helloName("Ali") == "Hello Ali!");
+    assert(helloName("David") == "Hello David!");
 
 
 	return 0;
 }
+
+string helloName(string name)
+{
+    
+    return "Hello " + name + "!";
+}
+
