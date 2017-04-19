862d766bddc8931090605a7cd10d885b4cc1c8b4
Becca <rhamman@stu.ctcd.edu>
2017-04-19 17:33:47 -0500

    Solved Java String-2

--- driver.cpp
+++ driver.cpp
@@ -3,26 +3,28 @@
 #include <cassert>
 using namespace std;
 
-string helloName(string name);
+//string helloName(string name);
+string makeAbba(string a, string b);
 
 int main()
-{
-	cout << "Hello World!" << endl;
-    cout << "This is a great day to code!" << endl;
+{   
+    assert(makeAbba("Hi", "Bye") == "HiByeByeHi");
+    assert(makeAbba("Yo", "Alice") == "YoAliceAliceYo");
+    assert(makeAbba("What", "Up") == "WhatUpUpWhat");
+    assert(makeAbba("Go", "Now") == "GoNowNowGo");
 
-    
-    assert(helloName("Bob") == "Hello Bob!");
-    assert(helloName("Alice") == "Hello Alice!");
-    assert(helloName("Ali") == "Hello Ali!");
-    assert(helloName("David") == "Hello David!");
 
 
 	return 0;
 }
 
-string helloName(string name)
+/*string helloName(string name)
 {
     
     return "Hello " + name + "!";
-}
+}*/
 
+string makeAbba(string a, string b)
+{
+    return a + b + b + a;
+}
